aa3c8835aecf88f9b72ef34f21171b74cd1a3274
jh642 <jh642@csserver.evansville.edu>
2019-02-21 15:57:51 -0600

    Attempt to start but idk

--- Select.c
+++ Select.c
@@ -8,13 +8,24 @@ SELECT Algorithm
 
 #include <stdlib>
 #include "Select.h"
-
-int Select(int *A, itn p, int r, int i){
-  
+/*
+< 3 4 3 44 55 | 2 1 566 33 21 45 | 44 21>
+*/
+int Select(int *A, int p, int r, int i){
+  if (r-p == 1)
+    return A[1];
+  int size = r - p + 1;
+  int 
+  for (int i = 0; i < (size/5)+1; i++){
+    
+  }
+  for (int i = 0; i < 5; i++){
+    
+  }
 }
 /*
     1. Divide the n-elements of input array into floor(n/5) groups of 5
-    elements each and at most one group of remaining lemenets n mod 5
+    elements each and at most one group of remaining elenets n mod 5
     2. Find the median of each of the ceiling(n/5) groups by using insertion
     sort and find medians of each group
     3. Use Select recursively to find the medians of the medians
@@ -25,4 +36,4 @@ int Select(int *A, itn p, int r, int i){
     5. If i=k, return x, else call recursively Select on lower and upper
     side depending on if i < k or i > k
     Note: lower median if even
-   */
+*/
