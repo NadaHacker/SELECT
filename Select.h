d8c604d065954279f9f790ad375b87f949705fa2
jh642 <jh642@csserver.evansville.edu>
2019-02-21 12:47:51 -0600

    Created header file

---
+++ Select.h
@@ -0,0 +1,9 @@
+/*
+Jared Holzmeyer
+Dr. Morse
+CS 315
+2/21/19
+SELECT Algorithm
+*/
+
+#include <stdlib>
---
+++ select.h
@@ -0,0 +1,16 @@
+/*
+Jared Holzmeyer
+Dr. Morse
+CS 315
+2/21/19
+SELECT Algorithm
+*/
+
+#ifndef SELECT_H_
+#define SELECT_H_
+
+int Select(int *A, int p, int r, int i);
+
+int Partition(int *A, int p, int r, int x);
+
+#endif
